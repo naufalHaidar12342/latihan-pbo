443fc7b5ed31401452e4f9b2ec151738318d9043
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-04-06 19:39:47 +0700

    memberikan spasi pada beberapa baris

--- konversiWaktu/KonversiWaktu/src/main/java/KonversiWaktu/KonversiWaktu.java
+++ konversiWaktu/KonversiWaktu/src/main/java/KonversiWaktu/KonversiWaktu.java
@@ -6,31 +6,30 @@
 package KonversiWaktu;
 
 import java.util.Scanner;
+
 /**
  *
  * @author GIGABYTE(github.com/naufalHaidar12342)
  */
 public class KonversiWaktu {
-   public static void main (String[] args){
-       
-        //memanggil fungsi scanner lewat variabel 'inputan'
+   public static void main(String[] args) {
+
+      // memanggil fungsi scanner lewat variabel 'inputan'
       Scanner inputan = new Scanner(System.in);
-      
-      //untuk menutup variabel 'inputan'
-      try{
-         //meminta inputan detik dari user
+
+      // untuk menutup variabel 'inputan'
+      try {
+         // meminta inputan detik dari user
          System.out.println("masukkan detik :");
-         int hasilInputan=inputan.nextInt();
-        
-         //memanggil class 'KonversiDetik' lewat 
-         //variabel 'konversiInput'
+         int hasilInputan = inputan.nextInt();
+
+         // memanggil class 'KonversiDetik' lewat
+         // variabel 'konversiInput'
          KonversiDetik konversiInput = new KonversiDetik(hasilInputan);
          konversiInput.Cetak();
-      }finally{
+      } finally {
          inputan.close();
       }
-        
-        
-   
+
    }
 }
